0ae29ec7228bb4f3aa00a6b786009f16fc1df19f
253732-rgb <253732@it2id.upchiapas.edu.mx>
2025-11-07 12:05:03 -0600

    Este es un archivo de c++

---
+++ pares.cpp
@@ -0,0 +1,15 @@
+#include  <iostream>
+using namespace std;
+
+int main(){
+	int i;
+	int resultado;
+	cout<< "numeros pares" << endl;
+	for (i=1;i<=30;i++){
+		resultado = 2+i;
+		cout<< "2" << i << "=" << resultado << endl;
+	}
+	
+	
+	return 0;
+}
